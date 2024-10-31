2bce94b0141bf538b8333488ad36eae82185bdd6
DwongBeTeo <duongdangtran54321@gmail.com>
2024-10-31 14:30:15 +0700

    TTCD1

---
+++ K22CNT2_TranDangDuong_2210900097/Controllers/DanhMucController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TranDangDuong_2210900097.Models;
+
+namespace TranDangDuong_2210900097.Controllers
+{
+    public class DanhMucController : Controller
+    {
+        private QuanLyBanHangEntities db = new QuanLyBanHangEntities();
+
+        // GET: DanhMuc
+        public ActionResult Index()
+        {
+            return View(db.DanhMucs.ToList());
+        }
+
+        // GET: DanhMuc/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DanhMuc danhMuc = db.DanhMucs.Find(id);
+            if (danhMuc == null)
+            {
+                return HttpNotFound();
+            }
+            return View(danhMuc);
+        }
+
+        // GET: DanhMuc/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: DanhMuc/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MaDanhMuc,TenDanhMuc")] DanhMuc danhMuc)
+        {
+            if (ModelState.IsValid)
+            {
+                db.DanhMucs.Add(danhMuc);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(danhMuc);
+        }
+
+        // GET: DanhMuc/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DanhMuc danhMuc = db.DanhMucs.Find(id);
+            if (danhMuc == null)
+            {
+                return HttpNotFound();
+            }
+            return View(danhMuc);
+        }
+
+        // POST: DanhMuc/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MaDanhMuc,TenDanhMuc")] DanhMuc danhMuc)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(danhMuc).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(danhMuc);
+        }
+
+        // GET: DanhMuc/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DanhMuc danhMuc = db.DanhMucs.Find(id);
+            if (danhMuc == null)
+            {
+                return HttpNotFound();
+            }
+            return View(danhMuc);
+        }
+
+        // POST: DanhMuc/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            DanhMuc danhMuc = db.DanhMucs.Find(id);
+            db.DanhMucs.Remove(danhMuc);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ K22CNT2_TranDangDuong_2210900097/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TranDangDuong_2210900097.MvcApplication" Language="C#" %>
